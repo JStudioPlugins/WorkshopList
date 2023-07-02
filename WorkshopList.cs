b16052ffded415da2d2a0384ae35a498068e336d
Jdance-Media <67397422+Jdance-Media@users.noreply.github.com>
2023-07-02 00:22:33 -0400

    Actual version.

--- WorkshopList.cs
+++ WorkshopList.cs
@@ -1,12 +1,163 @@
-﻿using System;
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using SDG.Provider;
+using SDG.Unturned;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using ShimmyMySherbet.DiscordWebhooks;
 using System.Threading.Tasks;
+using ShimmyMySherbet.DiscordWebhooks.Embeded;
+using Newtonsoft.Json;
 
 namespace WorkshopList
 {
-    public class WorkshopList
+    public class WorkshopList : RocketPlugin<WorkshopListConfig>
     {
+        public string WebhookURL
+        {
+            get
+            {
+                return Configuration.Instance.WebhookURL;
+            }
+        }
+
+        public ulong MessageId { get; private set; }
+
+        public string MessageEndpoint
+        {
+            get
+            {
+                return WebhookURL + $"/messages/{MessageId}";
+            }
+        }
+
+        public string DataFilePath
+        {
+            get
+            {
+                string dir = Path.Combine(Directory, "data");
+                if (!System.IO.Directory.Exists(dir)) System.IO.Directory.CreateDirectory(dir);
+                return Path.Combine(dir, "data.dat");
+            }
+        }
+
+
+        protected override void Load()
+        {
+            Logger.Log($"{Name} {Assembly.GetName().Version} has been loaded.");
+            MessageId = LoadMessageId();
+            Level.onPostLevelLoaded += HandlePostLoaded;
+        }
+
+        private void HandlePostLoaded(int level)
+        {
+            SendWorkshopList();
+        }
+
+        protected override void Unload()
+        {
+            Level.onPostLevelLoaded -= HandlePostLoaded;
+            Logger.Log($"{Name} has been unloaded.");
+        }
+
+        public void SendWorkshopList()
+        {
+            if (MessageExists(MessageId))
+            {
+                string content = string.Empty;
+                foreach (ulong id in Provider.getServerWorkshopFileIDs())
+                {
+                    TempSteamworksWorkshop.getCachedDetails(new Steamworks.PublishedFileId_t(id), out CachedUGCDetails details);
+                    content += $"\n{details.GetTitle()}" + $" ([{id}](https://steamcommunity.com/sharedfiles/filedetails/?id={details.fileId.m_PublishedFileId}))";
+                }
+
+                DiscordWebhookService.EditMessage(WebhookURL, MessageId, new WebhookMessage().PassEmbed().WithTitle($"Workshop Mods - {Provider.serverName}").WithDescription(content).WithColor(EmbedColor.SlateGray).WithTimestamp(DateTime.Now).Finalize());
+            }
+            else
+            {
+                try
+                {
+                    string content = string.Empty;
+                    foreach (ulong id in Provider.getServerWorkshopFileIDs())
+                    {
+                        TempSteamworksWorkshop.getCachedDetails(new Steamworks.PublishedFileId_t(id), out CachedUGCDetails details);
+                        content += $"\n{details.GetTitle()}" + $" ([{id}](https://steamcommunity.com/sharedfiles/filedetails/?id={details.fileId.m_PublishedFileId}))";
+                    }
+
+                    HttpWebRequest request = WebRequest.CreateHttp(WebhookURL + "?wait=true");
+                    request.Method = "POST";
+                    request.ContentType = "application/json";
+
+                    string Payload = JsonConvert.SerializeObject(new WebhookMessage().PassEmbed().WithTitle($"Workshop Mods - {Provider.serverName}").WithDescription(content).WithColor(EmbedColor.SlateGray).WithTimestamp(DateTime.Now).Finalize());
+                    byte[] Buffer = Encoding.UTF8.GetBytes(Payload);
+
+                    request.ContentLength = Buffer.Length;
+                    using (Stream write = request.GetRequestStream())
+                    {
+                        write.Write(Buffer, 0, Buffer.Length);
+                        write.Flush();
+                    }
+
+                    var resp = (HttpWebResponse)request.GetResponse();
+                    Stream receivingStream = resp.GetResponseStream();
+                    StreamReader readStream = new StreamReader(receivingStream, Encoding.UTF8);
+                    Message msg = JsonConvert.DeserializeObject<Message>(readStream.ReadToEnd());
+                    SaveMessageId(msg.id);
+
+                    /*using WebClient client = new();
+                    client.QueryString = new() { { "wait", "true" } };
+                    string str = client.UploadString(WebhookURL, JsonConvert.SerializeObject(new WebhookMessage().PassEmbed().WithTitle($"Workshop Mods - {Provider.serverName}").WithDescription(content).WithColor(EmbedColor.SlateGray).WithTimestamp(DateTime.Now).Finalize()));
+                    Message msg = JsonConvert.DeserializeObject<Message>(str);
+                    SaveMessageId(msg.id);*/
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogException(ex, "An exception occurred while attempting to execute a webhook.");
+                }
+            }
+        }
+
+        public ulong LoadMessageId()
+        {
+            string path = DataFilePath;
+            if (!File.Exists(path)) return ulong.MinValue;
+
+            Block block = ReadWrite.readBlock(path, false, false, 0);
+            return block.readUInt64();
+        }
+
+        public void SaveMessageId(ulong messageId)
+        {
+            Block block = new();
+            block.writeUInt64(messageId);
+            ReadWrite.writeBlock(DataFilePath, false, false, block);
+        }
+
+        public bool MessageExists(ulong messageId)
+        {
+            if (messageId == ulong.MinValue) return false;
+
+            try
+            {
+                using WebClient client = new();
+                client.DownloadData(MessageEndpoint);
+                Logger.Log("Found a proper message at the specified endpoint. Will edit that message.");
+                return true;
+            }
+            catch
+            {
+                Logger.Log("An exception occured when attempting to download a message at the specified endpoint. Likely going to send a new message.");
+                return false;
+            }
+        }
+    }
+
+    public class Message
+    {
+        public ulong id { get; set; }
     }
 }
--- WorkshopListConfig.cs
+++ WorkshopListConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rocket.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace WorkshopList
 {
-    class WorkshopListConfig
+    public class WorkshopListConfig : IRocketPluginConfiguration
     {
+        public string WebhookURL { get; set; }
+
+        public void LoadDefaults()
+        {
+            WebhookURL = "https://discord.com/api/webhooks/NULL/NULL";
+        }
     }
 }
